0c032f174de3738b0a01e0bd1198158426ed1dca
Kenny Koch <kennykoch47@gmail.com>
2022-04-08 15:09:57 -0400

    heroku deploy fix

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,6 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
     app.use(
-        createProxyMiddleware("/api/**", { target: "http://localhost:5000" })
+        createProxyMiddleware("/api/**", { target: process.env.PORT || "http://localhost:5000" })
     );
 };
