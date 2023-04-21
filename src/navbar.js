9ab55bf96298a2c3832dfc60b01a62e7b97650ad
Kenny Koch <kennykoch47@gmail.com>
2023-04-21 16:06:22 -0400

    navbar dropdown

--- src/navbar.js
+++ src/navbar.js
@@ -3,15 +3,14 @@
 import { Container, Navbar, Nav, NavDropdown } from 'react-bootstrap';
 function Navigation() {
     return (
-        <Navbar className='sticky-top' expand="lg"  >
+        <Navbar bg='dark' variant='dark' className='sticky-top' expand="lg"  >
             <Container>
-                <Navbar.Brand className='mx-auto' id='nav-title' href="#">NYC Cyclist and Pedestrian Safety Dashboard</Navbar.Brand>
+                <Navbar.Brand className='me-auto' id='nav-title' href="#">NYC Cyclist and Pedestrian Safety Dashboard</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
                     <Nav className="ml-auto">
-                        <Nav.Link href="#">Home</Nav.Link>
-                        <Nav.Link href="#">About</Nav.Link>
-                        <NavDropdown title="Statistics" id="basic-nav-dropdown">
+                        <Nav.Link href="#">Map</Nav.Link>
+                        <NavDropdown bg='dark' menuVariant='dark' title="Statistics" id="basic-nav-dropdown">
                             <NavDropdown.Item href="#">Item 1</NavDropdown.Item>
                             <NavDropdown.Item href="#">Item 2</NavDropdown.Item>
                             <NavDropdown.Item href="#">Item 3</NavDropdown.Item>
@@ -21,7 +20,7 @@ function Navigation() {
                     </Nav>
                 </Navbar.Collapse>
             </Container>
-        </Navbar>
+        </Navbar >
     )
 }
 
