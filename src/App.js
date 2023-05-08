281e44a31926ddccd37fb606ad093c04bc473273
Kenny Koch <kennykoch47@gmail.com>
2023-05-08 11:12:33 -0400

    adding cors to flask server

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
   useEffect(() => {
     console.log('hello from app')
     async function fetchData() {
-      const response = await fetch('/data')
+      const response = await fetch('http://localhost:5000/data')
       console.log('response', response)
       const jsonData = await response.json()
       console.log('jsonData', jsonData)
