726177e41421e7c5718c804d6ab930b0b8e276b0
Kenny Koch <kennykoch47@gmail.com>
2023-05-31 11:59:43 -0400

    using new api

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
   useEffect(() => {
     console.log('hello from app')
     async function fetchData() {
-      const response = await fetch('https://bike-safe-dashboard.herokuapp.com/data')
+      const response = await fetch('https://bike-safe-api.herokuapp.com/data')
       console.log('response', response)
       const jsonData = await response.json()
       console.log('jsonData', jsonData)
